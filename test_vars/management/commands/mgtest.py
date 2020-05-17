46ea9520d9adb6083a5b910cbef0cad2e7f2ed38
Mark Guthrie <mark.guthrie@abf.com.au>
2020-05-18 08:54:44 +1000

    print

--- test_vars/management/commands/mgtest.py
+++ test_vars/management/commands/mgtest.py
@@ -1,6 +1,8 @@
 from django.core.management.base import BaseCommand, CommandError
+import os
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
         self.stdout.write('fishcake - inside command')
+        os.system("env")
