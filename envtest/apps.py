7126ded90c6ef347bee2bce4112da3318a1f52f1
Mark Guthrie <mark.guthrie@abf.com.au>
2020-05-18 08:52:23 +1000

    print

--- envtest/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class EnvtestConfig(AppConfig):
-    name = 'envtest'
--- fishcake/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class FishcakeConfig(AppConfig):
-    name = 'fishcake'
